ad1c027bbfcbcc56507b52dcb1c51140a28cfb73
luan-services <luandotservices@gmail.com>
2025-09-04 01:24:10 -0300

    feat: added react-router and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+// import do react-router
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import das páginas
+import { HomePage } from './pages/HomePage';
+import { PicturePage } from './pages/PicturePage';
+
+// import do css global
 import './App.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        index: true, // "index: true" indica que este é o componente padrão para a rota pai "/"
+        element: <HomePage />,
+      },
+      {
+        path: "picture",
+        element: <PicturePage/>,
+      }
+    ],
+  },
+]);
+
 
 function App() {
 
 	return (
-		<>
-			<p>Hello</p>
-		</>
+		<div className="min-h-screen text-gray-700">
+			<RouterProvider router={router} />
+		</div>
 	)
 }
 
