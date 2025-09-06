6c3eec1fb55f1c6ed40ce6c067a9056724f2b565
luan-services <luandotservices@gmail.com>
2025-09-06 18:37:09 -0300

    feat: update FormButton added glass icon

--- components/home_page/FormButton.jsx
+++ components/home_page/FormButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 export const FormButton = ({children, ...props}) => {
 	return (
-		<button className="flex min-w-48 bg-custom-primary-red justify-center py-2 px-4 text-white font-medium rounded-xl cursor-pointer active:scale-92 transition disabled:active:scale-100 disabled:opacity-50 disabled:cursor-wait items-center" {...props}>
-			{children}
+		<button className="flex min-w-48 bg-custom-primary-red items center justify-center py-2 px-4 text-white font-medium rounded-xl cursor-pointer active:scale-92 transition disabled:active:scale-100 disabled:opacity-50 disabled:cursor-wait items-center" {...props}>
+			<FontAwesomeIcon icon={faSearch} className="mr-2" /> {children}
 		</button>
 	)
 }
