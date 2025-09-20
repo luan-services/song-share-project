44e0a659cb0fb37cbee7f4ba4ccd60d6ca0d5ac6
luan-services <luandotservices@gmail.com>
2025-09-20 10:58:26 -0300

    fix: update TermsFooter added credits to lrclib

--- components/TermsFooter.jsx
+++ components/TermsFooter.jsx
@@ -5,7 +5,7 @@ export const TermsFooter = () => {
 	return (
 		<div className='flex flex-col justify-center gap-1 p-4'>
 			<div className="flex flex-col md:flex-row items-center text-center justify-center gap-2">
-				<span className="font-medium">© 2025 Song Sticker. Todos os direitos reservados. </span>
+				<span translate='no' className="font-medium">© 2025 Song Sticker. Todos os direitos reservados. </span>
 				<span >
 					<Link to="/termos" className="cursor-pointer">Termos de Serviço </Link> | 
 					<Link to="/privacidade" className="cursor-pointer"> Políticas de Privacidade</Link>
@@ -15,7 +15,7 @@ export const TermsFooter = () => {
 
 			<div className="flex justify-center gap-2">
 				<p className="text-sm"> 
-					Dados fornecidos por <a href="https://developers.deezer.com/" target="_blank" rel="noopener noreferrer" className="underline">Deezer</a>.
+					Dados fornecidos por <a href="https://developers.deezer.com/" target="_blank" rel="noopener noreferrer" translate='no' className="underline">Deezer</a> e <a href="https://lrclib.net/" target="_blank" rel="noopener noreferrer" translate='no' className="underline">lrclib</a>.
 				</p>
 			</div>
 			
